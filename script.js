5d2f9af0121cd8b21763a9a6a943602a745c93d6
aidxelsystems <aidxelsystems@gmail.com>
2025-12-16 17:07:44 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbwY3c3S-CjsOomn-YW4jlJakDjxw78q85JZPd57WNhFKDkWOT-5wuJGp9GbRxehD02C/exec';
+const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbx9Mb4NJaRNxtKvY4zyA9F4t1HomHAPcsLahSp3Tb5cYIeqO37xlkX1ssNZ7rXkopbzBQ/exec';
 
 // 状態管理
 let currentUser = null;
