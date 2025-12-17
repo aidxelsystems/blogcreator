efeb4ed164a8137a256f754ffe7372240971139f
aidxelsystems <aidxelsystems@gmail.com>
2025-12-17 22:49:20 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbwiT-nGaJkw6be5YQ35N9DWxEzSIMQdFj9bohezKGEt_p3PR4SVQGZWNnIN5HqvfhFY5w/exec';
+const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbwpUVCpX6jTd4GzBpz5NNUPnHYte1i4AmGi9Nk7yflqSeDsZh8I3WS6bmeosY2gUGuNNg/exec';
